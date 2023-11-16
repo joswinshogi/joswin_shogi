598f0989a1a8a4fba5e835d39766fab4305991b8
Joswin_Shogi <104709945+joswinshogi@users.noreply.github.com>
2023-11-16 15:07:46 +0530

    udated-about

--- components/About.jsx
+++ components/About.jsx
@@ -22,36 +22,9 @@ const About = () => {
             needed. I believe that being a great developer is not using one
             specific language, but choosing the best tool for the job.
           </p>
-          <p className='py-2 text-gray-600'>
-            Enthusiastic about the transformative power of technology, I am Joswin Shogi, a BTech student pursuing a degree in Artificial Intelligence and Data Science. My passion for technology stems from its ability to solve real-world problems and make a positive impact on society.
-
-            Beyond my academic pursuits, I am an avid cricket fan, always eager to catch the latest matches and analyze the strategies employed by the players. Mathematics has also been a constant source of fascination for me, its logical beauty and problem-solving applications captivating my intellectual curiosity.
-
-            Coupling my academic knowledge with my programming skills, I am actively involved in developing software solutions. The process of translating ideas into functional code fuels my passion for programming, and I constantly seek opportunities to enhance my skills and explore new technologies.
-
-            Driven by a desire to contribute to the advancement of AI and data science, I am committed to continuous learning and exploration. I believe that technology holds the key to addressing many of the world's challenges, and I am eager to play a part in shaping its future.
-
-            My Interests:
-
-            Artificial Intelligence
-            Data Science
-            Cricket
-            Mathematics
-            Programming
-            My Goals:
-
-            Become an expert in AI and data science
-            Develop innovative solutions using AI and data science
-            Make a positive impact on society through technology
-            My Skills:
-
-            Programming (Python, Java, C++)
-            Data analysis
-            Machine learning
-            Problem-solving
-            Critical thinking
-            I am always looking for new challenges and opportunities to expand my knowledge and skills. Feel free to connect with me if you have any interesting projects or ideas.
-          </p>
+          {/* <p className='py-2 text-gray-600'>
+            
+          </p> */}
           {/* <Link href='/#projects'>
             <p className='py-2 text-gray-600 underline cursor-pointer'>
               Check out some of my latest projects.
